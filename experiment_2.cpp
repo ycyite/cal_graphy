94fe0f3848170d919aadc8c3a3f89b2c89eb44eb
础源 杨 <1337435263@qq.com>
2020-05-09 14:04:19 +0800

    文件

---
+++ experiment_2.cpp
@@ -0,0 +1,96 @@
+#include"pch.h"
+#include<iostream>
+#include<cmath>
+#include"experiment_2.h"
+using namespace std;
+Point::Point(double a, double b)
+{
+	x = a;
+	y = b;
+}
+Point::~Point()
+{
+}
+inline line::line(double a, double b, double c, double d)
+{
+	x1 = a;
+	y1 = b;
+	x2 = c;
+	y2 = d;
+}
+double line::length() const
+{
+	return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
+double line::area() const
+{
+	return 0;
+}
+line::~line()
+{
+}
+parallelogram::parallelogram(double a1, double b1, double a2, double b2, double a3, double b3, double a4, double b4)
+{
+	x3 = a1;
+	x4 = a2;
+	x5 = a3;
+	x6 = a4;
+	y3 = b1;
+	y4 = b2;
+	y5 = b3;
+	y6 = b4;
+}
+double parallelogram::length()const
+{
+	double a, b;
+	a = sqrt(pow((x3 - x4), 2) + pow((y3 - y4), 2));
+	b = sqrt(pow((x3 - x5), 2) + pow((y3 - y5), 2));
+	return (a + b) * 2;
+}
+double parallelogram::area()const
+{
+	double a, h, s, A, B;
+	a = sqrt(pow((x3 - x4), 2) + pow((y3 - y4), 2));
+	A = fabs((x5 - x3) / (x4 - x3));
+	B = -1;
+	h = fabs(fabs(A * (x5 - x4) + y4 - y5) / sqrt(pow(A, 2) + pow(B, 2)));
+	s = a * h;
+	return s;
+}
+parallelogram::~parallelogram()
+{
+}
+inline Circle::Circle(double x, double y, double R)
+{
+	x7 = x;
+	y7 = y;
+	radius = R;
+}
+inline void Circle::getradius(double R)
+{
+	radius = R;
+}
+inline double Circle::area() const
+{
+	return PI * radius * radius;
+}
+inline double Circle::length() const
+{
+	return 2 * PI * radius;
+}
+Arc::Arc(double a,double b,double c,double an):Circle(a,b,c),angle(an)
+{	
+}
+double Arc::length()const
+{
+	return (2*radius+radius*angle);
+}
+double Arc::area()const
+{
+	double area;
+	area = radius * radius * angle / 2;
+	return (area);
+}
+Arc::~Arc()//定义析构函数
+{
+}
---
+++ experiment_2.h
@@ -0,0 +1,81 @@
+#pragma once
+#include"pch.h"
+#include<iostream>
+using namespace std;
+#include<string>
+#define PI 3.1415926
+class Shape//定义基类
+{
+public:
+	virtual double area()const = 0;//声明求面积的虚函数
+	virtual double length()const = 0;//声明求周长的虚函数
+};
+class Point//定义Shape的派生类Point
+{
+public:
+	Point(double = 0, double = 0);//定义构造函数
+	double getX()const
+	{
+		return x;
+	}//获取X坐标的函数
+	double getY()const
+	{
+		return y;
+	}//获取Y坐标的函数
+	~Point();//定义析构函数
+	double x;
+	double y;
+};
+class line :public Point//定义Point的派生类line
+{
+protected:
+	double x1;
+	double x2;
+	double y1;
+	double y2;
+public:
+	line(double=0,double =0,double=0,double=0);//声明构造函数
+	virtual double length()const;//声明求周长的虚函数
+	virtual double area() const;//声明求面积的虚函数
+	~line();//定义析构函数
+};
+class parallelogram :public line//定义line类的派生类parallelogram
+{
+protected:
+	double x3;
+	double x4;
+	double y3;
+	double y4;
+	double x5;
+	double x6;
+	double y5;
+	double y6;
+public:
+	parallelogram (double = 0, double = 0, double = 0, double = 0, double = 0, double = 0,double=0,double =0) ;//声明构造函数
+	virtual double length()const;//声明求周长的虚函数,返还周长的数值
+	virtual double area()const;//声明求面积的虚函数，返还面积的数值
+	bool judge();//声明判断是否构成平行四边形的函数，如果是则返还真值，如果不是则返还假
+	~parallelogram();//定义析构函数
+};
+class Circle :public Point//定义Point的派生类Circle
+{
+protected:
+	double y7;
+	double x7;
+	double radius;
+public:
+	Circle(double=0,double=0,double=0);//声明构造函数
+	void getradius(double);//声明获取半径的函数
+	virtual double area()const;//声明求面积的虚函数，返还面积的数值
+	virtual double length()const;//声明求周长的虚函数，返还周长的数值
+};
+class Arc :public Circle//定义Circle类的派生类Arc
+{
+private:
+	double angle;
+public:
+	Arc(double=0,double=0, double=0,double=0);//定义构造函数
+	virtual double area()const;//声明求面积的虚函数，返还面积的数值
+	virtual double length()const;//声明求周长的虚函数，返还周长的数值
+	~Arc();//定义析构函数
+};
